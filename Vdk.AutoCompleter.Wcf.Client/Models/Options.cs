e51802295252e815fe668e6407370965395c5019
vanderkorn <vanderkorn@gmail.com>
2014-10-09 01:17:17 +0600

    fix client

---
+++ Vdk.AutoCompleter.Wcf.Client/Models/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Vdk.AutoCompleter.Wcf.Client.Models
+{
+    public class Options
+    {
+        [Option('h', "host", Required = true, HelpText = "Input host.")]
+        public string Host { get; set; }
+
+        [Option('p', "port", Required = true, HelpText = "Input port.")]
+        public string Port { get; set; }
+    }
+}
