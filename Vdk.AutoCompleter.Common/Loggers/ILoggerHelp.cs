1ee155affa7e2096c0f37f9f7fea0b70bbe2cede
vanderkorn <vanderkorn@gmail.com>
2014-10-17 00:37:08 +0600

    logger add

---
+++ Vdk.AutoCompleter.Common/Loggers/ILoggerHelp.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ILoggerHelp.cs" company="Ivan Kornilov">
+//   Copyright ©  2014, Ivan Kornilov. All rights reserved.
+// </copyright>
+// <summary>
+//   Interface NLOG help service
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Vdk.AutoCompleter.Common.Loggers
+{
+    using NLog;
+
+    using Vdk.AutoCompleter.Common.IOC;
+
+    /// <summary>
+    /// Interface logger help service
+    /// </summary>
+    public interface ILoggerHelp : IDependency
+    {
+        /// <summary>
+        /// Gets or sets logger
+        /// </summary>
+        Logger Logger { get; set; }
+    }
+}
---
+++ Vdk.AutoCompleter.Common/Loggers/NlogLoggerHelp.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NlogLoggerHelp.cs" company="Ivan Kornilov">
+//   Copyright ©  2014, Ivan Kornilov. All rights reserved.
+// </copyright>
+// <summary>
+//   NLOG help service
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Vdk.AutoCompleter.Common.Loggers
+{
+    using NLog;
+
+    /// <summary>
+    /// NLOG help service
+    /// </summary>
+    public class NlogLoggerHelp : ILoggerHelp
+    {
+        /// <summary>
+        /// Gets or sets NLOG Logger
+        /// </summary>
+        public Logger Logger { get; set; }
+    }
+}
